45d9dfd9fa4b68cec215c0c8bd992fb67444e220
chanm0 <mikec201722@gmail.com>
2019-01-28 10:13:08 -0800

    Register Controller test works

---
+++ tests/Unit/RegisterDomainTests/RegisterControllerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Mockery;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Auth\RegisterDomain\Contracts\RegisterContract;
+use App\Http\Controllers\Api\Auth\RegisterDomain\RegisterController;
+
+class RegisterControllerTest extends TestCase
+{
+    private $controller;
+    private $retriever;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->retriever = Mockery::mock(RegisterContract::class);
+        $this->controller = new RegisterController($this->retriever);
+    }
+
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_register_controller_with_mockery()
+    {
+        $input = [
+            "name" => "tes3t@email.com",
+            "email" => "tes3t@email.com",
+            "password" => "tes3t@email.com",
+            "password_confirmation" => "tes3t@email.com"
+        ];
+
+        $request = new Request($input);
+
+        $expectedResponse = [
+            "name" => "tes3t@email.com",
+            "email" => "tes3t@email.com",
+            "updated_at" => "2019-01-28 17:55:46",
+            "created_at" => "2019-01-28 17:55:46",
+            "id" => 3
+        ];
+
+        $this->retriever
+            ->shouldReceive('register')
+            ->with($request)
+            ->once()->andReturn($expectedResponse);
+
+        $response = $this->retriever->register($request);
+
+        $this->assertEquals($expectedResponse, $response);
+    }
+}
