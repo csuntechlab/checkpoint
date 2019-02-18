6100f02c2d0f2ec59dfd1c7187db1d85fc3b90ff
chanm0 <mikec201722@gmail.com>
2019-02-18 11:48:46 -0800

    able to run script

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 update_ip(){
   # Grab the current IP address of the machine and store it in a variable. Use SED to replace old IP with current IP
   CURRENT_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
-  sed -i "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$CURRENT_IP/g" .env
+  sed -i '' -e "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$CURRENT_IP/g" .env
 }
 
 # Additional arguments "up" or "down" will also bring up/down the docker stack
