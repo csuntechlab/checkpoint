72e4a4b43f5f1ed976ab7f74499265a91cfd9bde
digitalsoba <anthony@Ryzen.localdomain>
2019-02-27 14:02:22 -0800

    WSL script

--- start.sh
+++ start.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
 # Function to update IP address in .env file
-update_ip(){
+echo "Are you using a Windows or Unix/Linux based PC?"
+echo "Press 1 for Unix/Linux (This includes MacOS)"
+echo "Press 2 for Windows"
+read -p "Enter 1 or 2: " system_os
+
+# Function to grab 
+update_ip_unix(){
   # Grab the current IP address of the machine and store it in a variable. Use SED to replace old IP with current IP
-  CURRENT_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
-  sed -i '' -e "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$CURRENT_IP/g" .env
+  UNIX_CURRENT_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+  sed -i '' -e "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$UNIX_CURRENT_IP/g" .env
 }
 
-# Additional arguments "up" or "down" will also bring up/down the docker stack
-if [[ $1 == "up" ]]; then
-  docker-compose up -d
-  update_ip
-  echo "Started checkpoint dev environment..."
-  echo "IP address updated in .env file"
-elif [[ $1 == "down" ]]; then
-  docker-compose down
-  echo "Stopped checkpoint dev environment"
+update_ip_windows(){
+  WINDOWS_CURRENT_IP=`hostname -I | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`
+  PRIMARY_WINDOWS_IP=`echo $WINDOWS_CURRENT_IP | awk '{print $1}'`
+  sed -i '.env' -e "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$PRIMARY_WINDOWS_IP/g"
+}
+
+if [[ $system_os == 1 ]]; then
+  echo "You are using a Unix system"
+  update_ip_unix
+elif [[ $system_os = 2 ]]; then  
+  echo "You're using Windows, go buy a Mac or install Linux"
+  update_ip_windows
 else
-  update_ip
-  echo "IP address updated in .env file"
+  echo "Invalid input, select 1 or 2. Exiting script"
 fi
+
+
+# Additional arguments "up" or "down" will also bring up/down the docker stack
+# if [[ $1 == "up" ]]; then
+#   docker-compose up -d
+#   update_ip
+#   echo "Started checkpoint dev environment..."
+#   echo "IP address updated in .env file"
+# elif [[ $1 == "down" ]]; then
+#   docker-compose down
+#   echo "Stopped checkpoint dev environment"
+# else
+#   update_ip
+#   echo "IP address updated in .env file"
+# fi
