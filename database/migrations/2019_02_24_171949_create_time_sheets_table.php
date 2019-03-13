83148ebdb6f727860875fa27f84f3894e51f4d08
chanm0 <mikec201722@gmail.com>
2019-03-13 14:28:29 -0700

    updated time sheets table

--- database/migrations/2019_02_24_171949_create_time_sheets_table.php
+++ database/migrations/2019_02_24_171949_create_time_sheets_table.php
@@ -19,7 +19,8 @@ class CreateTimeSheetsTable extends Migration
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users');
-            $table->text('time_frame_id');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
             $table->text('completed_time_sheet')->nullable();
             $table->timestamps();
         });
