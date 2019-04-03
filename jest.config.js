7f778c0ee32909e2c02758589072a48fad001ef3
digitalsoba <anthony.inlavong@gmail.com>
2019-04-03 16:33:04 -0700

    Add babel + example vue test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  testRegex: 'tests/jest/.*.spec.js$'
+  testRegex: 'tests/jest/.*.spec.js$',
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
+  'transform': {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+  },
+  
 }
--- tests/jest/index.spec.js
+++ tests/jest/index.spec.js
@@ -1,3 +1,9 @@
-test('it works', () => {
-  expect(1 + 1).toBe(2)
+
+import { mount } from '@vue/test-utils'
+import App from '../../resources/src/js/App.vue'
+
+test('Test App.Vue mount', () => {
+  const wrapper = mount(App)
+
+  expect(wrapper.isVisible()).toBe(true)
 })
