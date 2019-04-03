a826556665bf4331532ac167fbdd11eb214b870d
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2019-04-02 17:47:52 -0700

    add more controller and service logic to registerAdmin and begin testing, not done

---
+++ app/Http/Controllers/Api/Auth/RegisterDomain/Services/RegisterAdminService.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Http\Controllers\Api\Auth\RegisterDomain\Services;
+
+use Illuminate\Support\Facades\Hash;
+
+//Models
+use App\User;
+use App\Models\Program;
+use App\Models\Organization;
+use App\Models\UserInvitation;
+
+//Exceptions
+use App\Exceptions\AuthExceptions\UserCreatedFailed;
+
+//Contracts
+use App\Http\Controllers\Api\Auth\RegisterDomain\Contracts\RegisterAdminContract;
+
+
+class RegisterAdminService implements RegisterContract
+{
+    public function registerAdminUser(
+        $name,
+        $email,
+        $password
+      ):User {
+        try{
+          $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+          ]);
+        } catch (\Exception $e) {
+          throw new UserCreatedFailed();
+        }
+
+        return $user;
+      }
+
+    public function registerOrganization(
+      $organization_name,
+      $address,
+      $logo
+      ):Organization {
+        try{
+          $organization = Organization::create([
+            'organization_name' => $organization_name,
+            'address' => get_object_vars($address),
+            'logo' => $logo
+          ]);
+        } catch (\Exception $e) {
+          throw new OrganizationCreatedFailed();
+        }
+
+        return $organization;
+      }
+}
---
+++ app/Providers/ApiProviders/Auth/RegisterAdminServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers\ApiProviders\Auth;
+
+use Illuminate\Support\ServiceProvider;
+
+class RegiserServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            'App\Http\Controllers\Api\Auth\RegisterDomain\Contracts\RegisterAdminContract',
+            'App\Http\Controllers\Api\Auth\RegisterDomain\Services\RegisterAdminService'
+        );
+    }
+}
