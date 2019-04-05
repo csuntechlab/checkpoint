3d14e687ffd3084c0dc24b68c2cee0a7db0ec70d
chanm0 <mikec201722@gmail.com>
2019-04-05 14:01:47 -0700

    able to seed users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use App\Models\Organization;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class)->create();
+        User::create([
+            'name' => 'Tim Brumbough',
+            'email' => 'Tim@email.com',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+            'email_verified_at' => now(),
+            'remember_token' => str_random(10),
+            'organization_id' => Organization::all()->random()->id
+
+        ]);
+        factory(User::class, 100)->create();
     }
 }
