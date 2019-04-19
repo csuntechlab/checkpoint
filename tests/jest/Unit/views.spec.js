ae7461d8bb41cc5906d04c212486316de6684589
digitalsoba <anthony.inlavong@gmail.com>
2019-04-19 09:43:25 -0700

    Add vue test + fix warnings

--- tests/jest/Unit/views.spec.js
+++ tests/jest/Unit/views.spec.js
@@ -1,7 +1,16 @@
 import { mount } from '@vue/test-utils'
 import login from '../../../resources/src/js/router/views/login/index.vue'
+import admin from '../../../resources/src/js/router/views/admin/index.vue'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
 
 test('Does login vue exist?', () => {
   var wrapper = mount(login)
+  expect(wrapper.isVisible()).toBe(true)
+})
+
+test('Does admin vue exist?', () => {
+  var wrapper = mount(admin)
   expect(wrapper.isVisible()).toBe(true)
 })
