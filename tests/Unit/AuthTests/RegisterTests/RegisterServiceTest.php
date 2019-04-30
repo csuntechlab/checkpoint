8f633d2cad13877ac2569989ad1a85135eedfa77
chanm0 <mikec201722@gmail.com>
2019-04-30 15:48:21 -0700

    fixed register service test

--- tests/Unit/AuthTests/RegisterTests/RegisterServiceTest.php
+++ tests/Unit/AuthTests/RegisterTests/RegisterServiceTest.php
@@ -3,11 +3,15 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Mockery;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 use App\Services\RegisterService;
 use App\Models\UserInvitation;
+
+// Invitations
+use App\ModelRepositoryInterfaces\UserModelRepositoryInterface;
 
 class RegisterServiceTest extends TestCase
 {
@@ -16,12 +20,11 @@ class RegisterServiceTest extends TestCase
 
     private $classPath = '\App\Services\RegisterService';
 
-
-
     public function setUp()
     {
         parent::setUp();
-        $this->service = new RegisterService();
+        $this->modelInterface = Mockery::mock(UserModelRepositoryInterface::class);
+        $this->service = new RegisterService($this->modelInterface);
         $this->seed('TimeCalculatorTypeSeeder');
         $this->seed('PayPeriodTypeSeeder');
         $this->seed('OrganizationSeeder'); //seeds org and settings
@@ -46,27 +49,28 @@ class RegisterServiceTest extends TestCase
         $password = "tes3t@email.com";
         $inviteCode = $userInvitation->invite_code;
 
-        $response = $this->service->register($name, $email, $password, $inviteCode);
+        $expectedResponse = [
+            "user" => [
+                "name" => $name,
+                "email" => $email
+            ],
+            "role" => [
+                "name" => "Employee"
+            ]
+        ];
 
-        $this->assertArrayHasKey('name', $response);
-        $this->assertArrayHasKey('email', $response);
-    }
-
-    public function test_register_service_fails_throws_an_exception_undefined_index()
-    {
-        $userInvitation = UserInvitation::all()->random();
-
-        $name = null;
-        $email = $userInvitation->email;
-        $password = "tes3t@email.com";
-        $inviteCode = $userInvitation->invite_code;
-
-
-        $this->expectException('App\Exceptions\AuthExceptions\UserCreatedFailed');
+        $this->modelInterface
+            ->shouldReceive('create')
+            ->with($name, $email, $password, $userInvitation->organization_id, $userInvitation->role_id)
+            ->once()->andReturn($expectedResponse);
 
         $response = $this->service->register($name, $email, $password, $inviteCode);
 
-        $this->assertArrayHasKey('message_error', $response);
+        $this->assertArrayHasKey('user', $response);
+        $this->assertArrayHasKey('name', $response['user']);
+        $this->assertArrayHasKey('email', $response['user']);
+        $this->assertArrayHasKey('role', $response);
+        $this->assertArrayHasKey('name', $response['role']);
     }
 
     public function test_getOrganizationIdByUserInvitation()
@@ -81,7 +85,9 @@ class RegisterServiceTest extends TestCase
         $function = 'getOrganizationIdByUserInvitation';
         $method = $this->get_private_method($this->classPath, $function);
         $response = $method->invoke($this->service, $email, $inviteCode);
+
         $this->assertNotNull($response);
-        $this->assertInternalType('string', $response);
+        $this->assertEquals($userInvitation, $response);
+        $this->assertInstanceOf(UserInvitation::class, $response);
     }
 }
