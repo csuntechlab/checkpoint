6bd86d6a9aa04222d80b06c96c031b38eaf91ec5
chanm0 <mikec201722@gmail.com>
2019-04-30 15:09:28 -0700

    fixed register controller test

--- tests/Unit/AuthTests/RegisterTests/RegisterAdminControllerTest.php
+++ tests/Unit/AuthTests/RegisterTests/RegisterAdminControllerTest.php
@@ -14,86 +14,88 @@ use App\Contracts\RegisterAdminContract;
 
 class RegisterAdminControllerTest extends TestCase
 {
-  use DatabaseMigrations;
-  private $controller;
-  private $retriever;
-  private $classPath = "\App\Http\Controllers\RegisterAdminController";
+    use DatabaseMigrations;
+    private $controller;
+    private $retriever;
+    private $classPath = "\App\Http\Controllers\RegisterAdminController";
 
-  public function setUp()
-  {
-    parent::setUp();
-    $this->retriever = Mockery::mock(RegisterAdminContract::class);
-    $this->controller = new RegisterAdminController($this->retriever);
-    $this->seed('PassportSeeder');
-    $this->seed('TimeCalculatorTypeSeeder');
-    $this->seed('PayPeriodTypeSeeder');
-    $this->seed('OrganizationSeeder');
-    $this->seed('RoleSeeder');
-    $this->seed('UsersTableSeeder');
-  }
+    public function setUp()
+    {
+        parent::setUp();
+        $this->retriever = Mockery::mock(RegisterAdminContract::class);
+        $this->controller = new RegisterAdminController($this->retriever);
+        $this->seed('PassportSeeder');
+        $this->seed('TimeCalculatorTypeSeeder');
+        $this->seed('PayPeriodTypeSeeder');
+        $this->seed('OrganizationSeeder');
+        $this->seed('RoleSeeder');
+        $this->seed('UsersTableSeeder');
+    }
 
-  public function test_register()
-  {
-    $request = [
-      'first_name' => "Mike",
-      'last_name' => "Chann",
-      'address_number' => "9423",
-      'street' => "Reseda Blvd",
-      'city' => "Northridge",
-      'state' => "CA",
-      'zip' => "91324",
-      'organization_name' => "META+LAB",
-      'logo' => "logo.jpg",
-      'email' => "MikaruuChann69@email.com",
-      'password' => "A_password"
-    ];
+    public function test_register()
+    {
+        $request = [
+            'first_name' => "Mike",
+            'last_name' => "Chann",
+            'address_number' => "9423",
+            'street' => "Reseda Blvd",
+            'city' => "Northridge",
+            'state' => "CA",
+            'zip' => "91324",
+            'organization_name' => "META+LAB",
+            'logo' => "logo.jpg",
+            'email' => "MikaruuChann69@email.com",
+            'password' => "A_password"
+        ];
 
-    $expectedResponse = [];
+        $expectedResponse = [];
 
-    $this->retriever
-      ->shouldReceive('register')
-      ->with($request)
-      ->once()
-      ->andReturn($expectedResponse);
+        $this->retriever
+            ->shouldReceive('register')
+            ->with($request)
+            ->once()
+            ->andReturn($expectedResponse);
 
-    $response = $this->retriever->register($request);
+        $response = $this->retriever->register($request);
 
-    $this->assertEquals($expectedResponse, $response);
-  }
+        $this->assertEquals($expectedResponse, $response);
+    }
 
-  public function test_registerAdmin_http_call()
-  {
-    $request = [
-      'first_name' => "John",
-      'last_name' => "Doe",
-      'address_number' => "9423",
-      'street' => "Reseda Blvd",
-      'city' => "Northridge",
-      'state' => "CA",
-      'zip_code' => "91324",
-      'country' => "United States",
-      'organization_name' => "META+LAB",
-      'email' => "JohnDoe@email.com",
-      'logo' => "logo.jpg",
-      'password' => "A_password",
-      'password_confirmation' => "A_password"
-    ];
+    public function test_registerAdmin_http_call()
+    {
+        $request = [
+            'first_name' => "John",
+            'last_name' => "Doe",
+            'address_number' => "9423",
+            'street' => "Reseda Blvd",
+            'city' => "Northridge",
+            'state' => "CA",
+            'zip_code' => "91324",
+            'country' => "United States",
+            'organization_name' => "META+LAB",
+            'email' => "JohnDoe@email.com",
+            'logo' => "logo.jpg",
+            'password' => "A_password",
+            'password_confirmation' => "A_password"
+        ];
 
-    $response = $this->json('POST', "/api/register_admin", $request);
-    $response = $response->getOriginalContent();
-    $response = json_encode($response);
-    $expectedResponse = [
-      'organization' => [
-        'organization_name' => "META+LAB",
-        'logo' => "logo.jpg"
-      ],
-      'user' => [
-        'name' => "John Doe",
-        'email' => "JohnDoe@email.com",
-      ],
-      'role' => "Admin"
-    ];
-    $expectedResponse = json_encode($expectedResponse);
-    $this->assertEquals($response, $expectedResponse);
-  }
+        $response = $this->json('POST', "/api/register_admin", $request);
+        $response = $response->getOriginalContent();
+        $response = json_encode($response);
+        $expectedResponse = [
+            "user" => [
+                "name" => "John Doe",
+                "email" => "JohnDoe@email.com",
+            ],
+            "role" => [
+                "name" => "Admin"
+            ],
+            "organization" => [
+                "organization_name" => "META+LAB",
+                "logo" => "logo.jpg",
+            ],
+        ];
+        $expectedResponse = json_encode($expectedResponse);
+        $this->assertEquals($response, $expectedResponse);
+    }
 }
